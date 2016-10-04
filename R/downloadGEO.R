1a744733b9e0b8f0389f422a5a3dc0d69832b59c
Carlos Ruiz-Arenas <cruiz@creal.cat>
2016-10-04 12:49:51 +0000

    Comment downloadGEO Remove as_list Bump to version 1.1.6
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MultiDataSet@121733 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/downloadGEO.R
+++ R/downloadGEO.R
@@ -1,22 +1,21 @@
-#' Creation of a MultiDataSet with content of GEO GSE tags
-#'
-#' Given a vector of GSE tags with a single datasets. The function downloads
-#' the dataset. It converts the downloaded \code{ExpressionSet} to the
-#' correct set (\code{ExpressionSet} for expression data, \code{SnpSet} for
-#' SNP data and \code{MethylationSet} for methylation data) and it
-#' creates a MultiDataSet with them.
-#'
-#' @param gse_list String vector with the GSE tags.
-#' @param gse_type String vector with the GES data types (allowed:
-#' code{"expression"}, \code{"snp"} and \code{"methylation"}).
-#' @examples
-#' \dontrun{
-#' downloadGEO(
-#' 	gse_list = c("GSE40576", "GSE40732"), 
-#' 	gse_type = c("methylation", "expression")
-#' )
-#' }
-#' @export
+# #' Creation of a MultiDataSet with content of GEO GSE tags
+# #' 
+# #' Given a vector of GSE tags with a single datasets. The function downloads
+# #' the dataset. It converts the downloaded \code{ExpressionSet} to the
+# #' correct set (\code{ExpressionSet} for expression data, \code{SnpSet} for
+# #' SNP data and \code{MethylationSet} for methylation data) and it
+# #' creates a MultiDataSet with them.
+# #' 
+# #' @param gse_list String vector with the GSE tags.
+# #' @param gse_type String vector with the GES data types (allowed:
+# #' code{"expression"}, \code{"snp"} and \code{"methylation"}).
+# #' @examples
+# #' \dontrun{
+# #' downloadGEO(
+# #' 	gse_list = c("GSE40576", "GSE61989"),
+# #' 	gse_type = c("methylation", "expression")
+# #' )
+# #' }
 downloadGEO <- function(gse_list, gse_type) {
 	if(! "GEOquery" %in% utils::installed.packages()[,"Package"]) {
 		stop("Required package 'GEOquery' not installed.")
@@ -25,17 +24,22 @@ downloadGEO <- function(gse_list, gse_type) {
 	}
 	## we should check that the gse_list only contains gse tags
 	gse_sets <- lapply(gse_list, function(gse) {
-	    GEOquery::getGEO(gse, GSEMatrix = TRUE)[[1]]
+	    GEOquery::getGEO(gse, GSEMatrix = TRUE, AnnotGPL=FALSE)[[1]]
 	})
 	md <- createMultiDataSet()
 	rr <- lapply(1:length(gse_sets), function(ii) {
 		if(gse_type[[ii]] == "expression") {
+		    fdata <- featureData(gse_sets[[ii]])
+		    varLabels(fdata)[c(4, 7, 8)] <- c("chromosome", "start", "end")
+		    featureData(gse_sets[[ii]]) <- fdata
 			md <- add_genexp(md, gse_sets[[ii]])
 		} else if(gse_type[[ii]] == "methylation") {
-			meth <- methylationSet(
+		    fdata <- featureData(gse_sets[[ii]])
+			varLabels(fdata)[c(12, 13)] <- c("chromosome", "position")
+		    meth <- methylationSet(
 				exprs(gse_sets[[ii]]),
 				phenoData(gse_sets[[ii]]),
-				featureData(gse_sets[[ii]]),
+				fdata,
 				annotation(gse_sets[[ii]])
 			)
 			md <- add_methy(md, meth)
@@ -57,9 +61,9 @@ downloadGEO <- function(gse_list, gse_type) {
 
 # This SuperSeries is composed of the following SubSeries:
 # GSE40576	DNA Methylation Changes and Childhood Asthma in the Inner City [methylation]
-# GSE40732	DNA Methylation Changes and Childhood Asthma in the Inner City [gene expression]
+# GSE61989	YAP transcriptional regulator depletion effect on endothelial cells 
 
 
 # gse.m <- GEOquery::getGEO("GSE40576", GSEMatrix = TRUE)
-# gse.g <- GEOquery::getGEO("GSE40732", GSEMatrix = TRUE)
+# gse.g <- GEOquery::getGEO("GSE61989", GSEMatrix = TRUE)
 # gse.s <- GEOquery::getGEO("GSE53261", GSEMatrix = TRUE)

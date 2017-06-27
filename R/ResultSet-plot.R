edf032b0182ff79048d057b17e91f8e4603bf7a3
Carlos Ruiz <yocra3@gmail.com>
2017-06-27 14:39:13 +0200

    Bump version Allow subsetting and highlighting a Manhattan plot

--- R/ResultSet-plot.R
+++ R/ResultSet-plot.R
@@ -7,6 +7,8 @@
 #' @param type Type of plot to be drawn
 #' @param tPV Threshold for P-Value
 #' @param tFC Threshold for log FC of effect
+#' @param subset GenomicRanges used to zoom a region in Manhattan plot
+#' @param highlight GenomicRanges used to highlight a region in Manhattan plot
 #' @param show.effect (default: \code{TRUE}). Used in volcano plot. If \code{TRUE}, 
 #' effect is shown as FC instead of logFC. 
 #' @param show.lambda (default: \code{TRUE}) If \code{TRUE} shows lambda
@@ -18,7 +20,8 @@ setMethod(
     definition = function(x, y, rid = 1, coef = 2, contrast = NULL, type, 
                           tFC = 2, tPV = -log10(0.001),
                           show.effect = FALSE, show.lambda = TRUE,
-                          fNames = c("chromosome", "start"), ...) {
+                          fNames = c("chromosome", "start"), 
+                          subset, highlight, ...) {
         
         if (class(x@results[[rid]]$result) != "MArrayLM"){
             stop("plot function is only available for results in a MArrayLM object.")
@@ -37,7 +40,7 @@ setMethod(
             # Select columns P.Value, Chromosome and "Position"
             dta <- dta[ , c("P.Value", fNames)]
             # Add column with rs name per SNP
-            dta$SNP <- dta
+            dta$SNP <- rownames(dta)
             # Update colnames of 'dta' by the ones used on qqman::manhattan
             colnames(dta) <- c("P", "CHR", "BP", "SNP")
             # Two steps:
@@ -53,8 +56,23 @@ setMethod(
             # Convert both pposition and chromosome to numeric
             dta$CHR <- as.numeric(dta$CHR)
             dta$BP <- as.numeric(dta$BP)
-            # Plot using qqman
-            qqman::manhattan(dta, ylab = "-log10(P.Value)", ...)
+            
+            if (!missing(subset)){
+                gr <- GenomicRanges::makeGRangesFromDataFrame(
+                    dta, seqnames.field = "CHR", start.field = "BP", end.field = "BP")
+                gr <- IRanges::subsetByOverlaps(gr, subset)
+                dta <- dta[names(gr), ]
+            }
+            
+            if (!missing(highlight)){
+                gr <- GenomicRanges::makeGRangesFromDataFrame(
+                    dta, seqnames.field = "CHR", start.field = "BP", end.field = "BP")
+                gr <- IRanges::subsetByOverlaps(gr, highlight)
+                qqman::manhattan(dta, ylab = "-log10(P.Value)", highlight = names(gr), ...)
+            } else {
+                # Plot using qqman
+                qqman::manhattan(dta, ylab = "-log10(P.Value)", ...)
+            }
         } else if(type == "volcano") {
             dta <- getAssociation(x, rid = rid, coef = coef, contrast = contrast,
                                   fNames = NULL)

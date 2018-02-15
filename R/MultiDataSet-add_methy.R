92cd539ce2e9d72832b25519e92dd851364fba62
yocra3 <carlos.ruiz@isglobal.org>
2018-02-15 15:19:38 +0100

    Version 1.7.2 Solve problems with documentation End adaptation to new brgedata

--- R/MultiDataSet-add_methy.R
+++ R/MultiDataSet-add_methy.R
@@ -1,6 +1,8 @@
 #' @describeIn MultiDataSet Method to add a slot of methylation to 
 #' \code{MultiDataSet} from a \code{GenomicRatioSet}.
 #' @aliases MultiDataSet-methods
+#' @param methySet \code{GenomicRatioSet} to be used to fill the slot.
+#' @param ... Further arguments passed to add_rse or add_se
 setMethod(
     f = "add_methy",
     signature = c("MultiDataSet", "GenomicRatioSet"),
